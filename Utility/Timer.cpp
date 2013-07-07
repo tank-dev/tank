f62cbd49dba5a59a724cf52fc344d245cfed6393
Anstow <dvd_anstow@hotmail.co.uk>
2013-07-07 18:58:15 +0100

    Whops, inverted commas instead of <

--- Utility/Timer.cpp
+++ Utility/Timer.cpp
@@ -20,7 +20,7 @@
 #include "Timer.hpp"
 
 #if defined(__WIN32__) || defined(_WIN32) || defined(WIN32) || defined(__WINDOWS__) || defined(__TOS_WIN__)
-#include "windows.h"
+#include <windows.h>
 #else
 #include <thread>
 #endif
