8ad8c09ccfdf7b14be9b6b1e28b73c75deb9d9d3
Gazok <jamiebayne@hotmail.com>
2013-11-02 18:20:04 +0000

    Added Copyright

--- Tank/Utility/observing_ptr.hpp
+++ Tank/Utility/observing_ptr.hpp
@@ -1,3 +1,22 @@
+/* This file is part of Tank.
+ *
+ * Tank is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tank is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License and
+ * the GNU Lesser General Public Licence along with Tank. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2013 (©) Jamie Bayne, David Truby, David Watson.
+ */
+
 #ifndef TANK_OBSERVING_PTR
 #define TANK_OBSERVING_PTR
 #include <memory>
