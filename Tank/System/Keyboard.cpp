07420380b336e1791814b9d924c8c5c0d0cbfac0
David Truby <david.truby@gmail.com>
2014-02-21 23:15:46 +0000

    Relicenced to Boost Software Licence.

--- Tank/System/Keyboard.cpp
+++ Tank/System/Keyboard.cpp
@@ -1,21 +1,7 @@
-/* This file is part of Tank.
- *
- * Tank is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Tank is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License and
- * the GNU Lesser General Public Licence along with Tank. If not, see
- * <http://www.gnu.org/licenses/>.
- *
- * Copyright 2013 (©) Jamie Bayne, David Truby, David Watson.
- */
+// Copyright (©) Jamie Bayne, David Truby, David Watson 2013-2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
 
 #include "Keyboard.hpp"
 
