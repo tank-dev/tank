a93681c2d3c2940c67149d9bbef5f0e2bb3d2540
Anstow <dvd_anstow@hotmail.co.uk>
2014-05-04 16:18:29 +0100

    Added A* algorithm to collision grid

---
+++ Tank/Utility/CollisionGrid.cpp
@@ -0,0 +1,121 @@
+// Copyright (©) Jamie Bayne, David Truby, David Watson 2013-2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#include "CollisionGrid.hpp"
+
+namespace tank
+{
+
+// Retraces the path until we reach the start then reverse it
+inline std::vector<Vectoru> retracePath(const std::unordered_map<Vectoru, std::pair<Vectoru, float>>& cameFrom, const Vectoru& start, Vectoru currentNode)
+{
+    std::vector<Vectoru> path{currentNode};
+
+    auto nextNode = cameFrom.find(currentNode);
+    while (nextNode->second.first != start)
+    {
+        currentNode = nextNode->second.first;
+        path.push_back(currentNode);
+        nextNode = cameFrom.find(currentNode);
+    }
+
+    std::reverse(path.begin(), path.end());
+
+    return path;
+}
+
+std::vector<Vectoru> CollisionGrid::getPath(const Vectoru& start, const Vectoru& end)
+{
+    // This use's the A* algorithm to find a path between to points on the grid.
+
+    if (start == end)
+    {
+        // We are looking for a path to the same place we don't need no algorithm
+        return std::vector<Vectoru>{start};
+    }
+    std::unordered_set<Vectoru> closed;
+    // A multimap of nodes with key the huristic score for the node.
+    std::multimap<float, Vectoru> open;
+    open.insert(std::make_pair(pathHeuristic(start, end), start));
+
+    // A map of points to (where they came from, the cost using that connection)
+    std::unordered_map<Vectoru, std::pair<Vectoru, float>> cameFrom;
+    cameFrom.insert(std::make_pair(start, std::make_pair(start, 0)));
+
+    auto currentNode = open.upper_bound(-1.0f);
+    // Look until we've ran out of places to look
+    while (currentNode != open.end())
+    {
+        // If we've already dealt with this node remove it. I don't think this
+        // should ever happen though.
+        //
+        // Having this here is to aviod having to search and remove old values
+        // for an open point if we find new ones.
+        if (closed.find(currentNode->second) != closed.end())
+        {
+            open.erase(currentNode);
+        }
+        else
+        {
+            Vectoru point = currentNode->second;
+            if (point == end)
+            {
+                // We have found a path so return it.
+                return retracePath(cameFrom, start, end);
+            }
+            float costSoFar = cameFrom.find(point)->second.second;
+
+            if (point.y != 0)
+            {
+                checkDirection(closed, open , cameFrom, end, point, Vectoru{0,-1}, costSoFar);
+            }
+            if (point.y + 1 != getHeight())
+            {
+                checkDirection(closed, open , cameFrom, end, point, Vectoru{0,1}, costSoFar);
+            }
+            if (point.x != 0)
+            {
+                checkDirection(closed, open , cameFrom, end, point, Vectoru{-1,0}, costSoFar);
+                if (point.y != 0)
+                {
+                    checkDirection(closed, open , cameFrom, end, point, Vectoru{-1,-1}, costSoFar);
+                }
+                if (point.y + 1 != getHeight())
+                {
+                    checkDirection(closed, open , cameFrom, end, point, Vectoru{-1,1}, costSoFar);
+                }
+            }
+            if (point.x + 1 != getWidth())
+            {
+                checkDirection(closed, open , cameFrom, end, point, Vectoru{1,0}, costSoFar);
+                if (point.y != 0)
+                {
+                    checkDirection(closed, open , cameFrom, end, point, Vectoru{1,-1}, costSoFar);
+                }
+                if (point.y + 1 != getHeight())
+                {
+                    checkDirection(closed, open , cameFrom, end, point, Vectoru{1,1}, costSoFar);
+                }
+            }
+        }
+        // Go to the next node
+        currentNode = open.upper_bound(-1.0f);
+    }
+
+    // We have failed in finding a path return the empty path
+    return std::vector<Vectoru>{};
+}
+
+float CollisionGrid::pathHeuristic(const Vectorf& start, const Vectorf& end)
+{
+    return (start - end).magnitude();
+}
+
+float CollisionGrid::getCost(const Vectorf& start, const Vectorf& end)
+{
+    return (start - end).magnitude();
+}
+
+} // namespace tank
---
+++ Tank/Utility/CollisionGrid.hpp
@@ -0,0 +1,105 @@
+// Copyright (©) Jamie Bayne, David Truby, David Watson 2013-2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef TANK_COLLISIONGRID_HPP
+#define TANK_COLLISIONGRID_HPP
+
+#include "Grid.hpp"
+
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+
+namespace tank
+{
+
+/*!
+ * /brief This stores bool grid for collisions
+ */
+class CollisionGrid : public Grid<bool>
+{
+    void checkDirection(
+            const std::unordered_set<Vectoru>& closed,
+            std::multimap<float, Vectoru>& open,
+            std::unordered_map<Vectoru, std::pair<Vectoru, float>>& cameFrom,
+            Vectoru const& end, const Vectoru& point, const Vectoru& direction,
+            float costSoFar);
+
+public:
+    CollisionGrid(const Vectoru& dims);
+    CollisionGrid(const Vectoru& dims, bool intialValue);
+
+    template<typename T>
+    CollisionGrid(const Grid<T>& g, const std::vector<T>& collidable);
+
+    template<typename T>
+    void loadFromGrid(const Grid<T>& g, const std::vector<T>& collidable);
+
+    std::vector<Vectoru> getPath(const Vectoru& start, const Vectoru& end);
+
+    float pathHeuristic(const Vectorf& start, const Vectorf& end);
+    float getCost(const Vectorf& start, const Vectorf& end);
+};
+
+template<typename T>
+CollisionGrid::CollisionGrid(const Grid<T>& g, const std::vector<T>& collidable)
+    : Grid(g.getDimensions())
+{
+    loadFromGrid(g, collidable);
+}
+
+template<typename T>
+void CollisionGrid::loadFromGrid(const Grid<T>& g, const std::vector<T>& collidable)
+{
+    if (getDimensions() != g.getDimensions())
+    {
+        throw std::out_of_range("The grid must be the same size as the collision grid");
+    }
+
+    size_t size = getWidth() * getHeight();
+    for (size_t i = 0; i < size; ++i)
+    {
+        operator[](i) = (std::find(collidable.begin(), collidable.end(), g[i]) != collidable.end());
+    }
+}
+
+// This is run on every node to add new points to the open set.
+//
+// It is written as a lambda to make it easier to change if necessary and
+// to avoid having to input lots of parameters.
+void CollisionGrid::checkDirection(
+        const std::unordered_set<Vectoru>& closed,
+        std::multimap<float, Vectoru>& open,
+        std::unordered_map<Vectoru, std::pair<Vectoru, float>>& cameFrom,
+        Vectoru const& end, const Vectoru& point, const Vectoru& direction,
+        float costSoFar)
+{
+    Vectoru checkPoint = point + direction;
+    // check that the point isn't closed and we can travel through
+    if (closed.find(checkPoint) == closed.end() and operator[](checkPoint))
+    {
+        float newCostSoFar = costSoFar + getCost(point, checkPoint);
+        // Add the point to the open points
+        auto pre = cameFrom.find(checkPoint);
+        if (pre == cameFrom.end())
+        {
+            // If we don't already have a path for the point add one
+            cameFrom.insert(std::make_pair(checkPoint, std::make_pair(point, newCostSoFar)));
+            open.insert(std::make_pair(newCostSoFar + pathHeuristic(checkPoint, end), checkPoint));
+        }
+        else if (pre->second.second >= newCostSoFar)
+        {
+            // If the new path is shorter than the previously found path replace it
+            pre->second.first = point;
+            pre->second.second = newCostSoFar;
+            open.insert(std::make_pair(newCostSoFar + pathHeuristic(checkPoint, end), checkPoint));
+        }
+    }
+};
+
+} // namespace tank
+
+#endif // TANK_COLLISIONGRID_HPP
