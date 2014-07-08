2522466177d5d492ab23bae8ed61f5693d842225
Gazok <jamiebayne@hotmail.com>
2014-07-08 13:45:10 +0100

    Documented Logger

--- Tank/Utility/Logger.hpp
+++ Tank/Utility/Logger.hpp
@@ -34,6 +34,39 @@ public:
     }
 };
 
+/*!
+ * \brief A stream for logging debug information to a file.
+ *
+ * This acts like a standard ostream (_e.g._ `std::cout`).
+ * If `DEBUG` is defined, it also outputs to `std::clog`.
+ *
+ * Tank writes to Game::log when initializing and closing, so `log.txt` can
+ * show you if the engine is failing at some point.
+ *
+ * **NB:** remember to finish your log with `std::endl`.
+ *
+ * Example code:
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+ *     MyEnt() : tank::Entity({0, 0})
+ *     {
+ *         Game::log << "Created MyEnt at position (0, 0)" << std::endl;
+ *     }
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * Example `log.txt`:
+ *
+ *     [00:00:00.231] Log file created
+ *     [00:00:00.400] Opening Window
+ *     [00:00:00.641] Loading main state
+ *     [00:00:00.700] Loading World...
+ *     [00:00:00.710] Created MyEnt at position (0, 0)
+ *     [00:00:00.714] Entering main loop
+ *     [00:00:33.227] Unloading World...
+ *     [00:00:33.230] Closing Window
+ *     [00:00:33.233] Closing log file
+ *
+ */
 class Logger : public std::ostream
 {
     std::string fileName_;
