1984c17dabad1ecfcd11973d75beb420473710ba
David Truby <david.truby@gmail.com>
2015-01-15 10:34:34 +0000

    Added documentation for Music.

--- Tank/Audio/Music.cpp
+++ Tank/Audio/Music.cpp
@@ -21,12 +21,15 @@ bool Music::load(std::string fileName)
         return loaded_;
     }
     loaded_ = music_->openFromFile(fileName);
+    if (!loaded_) {
+        throw std::runtime_error("Failed to load music file");
+    }
     return loaded_;
 }
 
 void Music::play()
 {
-    if (music_) {
+    if (loaded_) {
         music_->play();
     } else {
         Game::log << "Attempt to play empty music" << std::endl;
@@ -35,7 +38,7 @@ void Music::play()
 
 void Music::pause()
 {
-    if (music_) {
+    if (loaded_) {
         music_->pause();
     } else {
         Game::log << "Attempt to pause empty music" << std::endl;
@@ -44,10 +47,50 @@ void Music::pause()
 
 void Music::stop()
 {
-    if (music_) {
+    if (loaded_) {
         music_->stop();
     } else {
         Game::log << "Attempt to stop empty music" << std::endl;
+    }
+}
+
+void Music::setVolume(float v)
+{
+    if (loaded_) {
+        music_->setVolume(v);
+    } else {
+        Game::log << "Attempt to set volume on empty music" << std::endl;
+    }
+}
+
+float Music::getVolume()
+{
+    if (loaded_) {
+        return music_->getVolume();
+    } else {
+        Game::log << "Attempt to get volume on empty music" << std::endl;
+        // Return default value.
+        return {};
+    }
+}
+
+void Music::setLoop(bool loop)
+{
+    if (loaded_) {
+        music_->setLoop(loop);
+    } else {
+        Game::log << "Attempt to set loop on empty music" << std::endl;
+    }
+}
+
+bool Music::getLoop()
+{
+    if (loaded_) {
+        return music_->getLoop();
+    } else {
+        Game::log << "Attempt to get loop on empty music" << std::endl;
+        // Return default value.
+        return {};
     }
 }
 }
--- Tank/Audio/Music.hpp
+++ Tank/Audio/Music.hpp
@@ -18,23 +18,121 @@ class Music
     bool loaded_ = false;
 
 public:
+    /*!
+     * \brief Construct an empty Music.
+     *
+     * Playing an empty music will do nothing and print a warning, so before
+     * attempting to play either Music::load() the file or use the other
+     * constructor.
+     */
     Music() = default;
+
+    /*!
+     * \brief Construct a Music from the file at the given path.
+     *
+     * This loads the music file at the given filename. It has the same effect
+     * as constructing an empty Music and then calling Music::load() on it.
+     *
+     * Supports the following formats: ogg, wav, flac, aiff, au, raw, paf, svx,
+     * nist, voc, ircam, w64, mat4, mat5 pvf, htk, sds, avr, sd2, caf, wve,
+     * mpc2k, rf64.
+     */
     Music(std::string fileName);
+
+    /*!
+     * \brief Default copy constructor.
+     *
+     * This shallow copies the music file, so is effectively cost-free.
+     */
     Music(Music const&) = default;
+
+    /*!
+     * \brief Default copy assignment operator.
+     *
+     * This shallow copies the music file, so is effectively cost-free.
+     */
     Music& operator=(Music const&) = default;
 
+    /*!
+     * \brief Load a music file from the given path.
+     *
+     * Supports the following formats: ogg, wav, flac, aiff, au, raw, paf, svx,
+     * nist, voc, ircam, w64, mat4, mat5 pvf, htk, sds, avr, sd2, caf, wve,
+     * mpc2k, rf64.
+     *
+     * \param fileName The path to load the music file from.
+     * \return false if the file failed to load, true otherwise
+     */
     bool load(std::string fileName);
+
+    // TODO: Why does this exist?
+    /*!
+     * \brief See: Music::load()
+     */
     bool loadFromFile(std::string fileName) {return load(fileName);}
 
+    /*!
+     * \brief Play the contained music.
+     *
+     * If the music is empty, logs that fact and does nothing.
+     */
     void play();
+
+    /*!
+     * \brief Pause the contained music.
+     *
+     * If the music is empty, logs that fact and does nothing.
+     */
     void pause();
+
+    /*!
+     * \brief Stop the contained music.
+     *
+     * If the music is empty, logs that fact and does nothing.
+     */
     void stop();
 
-    void setVolume(float v) { music_->setVolume(v); }
-    float getVolume() { return music_->getVolume(); }
-    void setLoop(bool loop) { music_->setLoop(loop); }
-    bool getLoop() { return music_->getLoop(); }
+    /*!
+     * \brief Set the volume of the music.
+     *
+     * If the music is empty, logs that fact and does nothing.
+     *
+     * \param v The volume of the music as a value between 0 and 100.
+     */
+    void setVolume(float v);
 
+    /*!
+     * \brief Get the volume of the music.
+     *
+     * If the music is empty, logs that fact and does nothing.
+     *
+     * \return The current volume of the music as a value between 0 and 100.
+     */
+    float getVolume();
+
+    /*!
+     * \brief Set whether the music will loop or not.
+     *
+     * If the music is empty, logs that fact and does nothing.
+     *
+     * \param loop `true` if music looping is desired, `false` otherwise.
+     */
+    void setLoop(bool loop);
+
+    /*!
+     * \brief Get whether the music will loop or not.
+     *
+     * If the music is empty, logs that fact and does nothing.
+     *
+     * \return `true` if music will loop, `false` otherwise.
+     */
+    bool getLoop();
+
+    /*!
+     * \brief Check if the music is loaded.
+     *
+     * \return `true` if music is loaded, `false` otherwise.
+     */
     explicit operator bool()
     {
         return loaded_;
