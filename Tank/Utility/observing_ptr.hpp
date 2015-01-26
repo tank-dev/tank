0a78c5e6850af66ea666f182eb7dc38a970d3a1f
David Truby <david.truby@gmail.com>
2015-01-26 19:28:04 +0000

    Initial testing and documentation on observing_ptr.

--- Tank/Utility/observing_ptr.hpp
+++ Tank/Utility/observing_ptr.hpp
@@ -7,6 +7,8 @@
 #define TANK_OBSERVING_PTR
 #include <memory>
 #include <functional>
+#include <type_traits>
+#include <boost/operators.hpp>
 
 namespace tank
 {
@@ -22,8 +24,10 @@ template <typename T>
 class observing_ptr
 {
     friend struct std::hash<observing_ptr>;
-    template <typename U>
-    friend class observing_ptr;
+
+    template <typename U> using not_bool_convertible =
+        std::enable_if_t<!std::is_convertible<U, bool>::value, T>;
+
     T* p_ = nullptr;
 
 public:
@@ -33,8 +37,24 @@ public:
      */
     observing_ptr() = default;
 
-    /// TODO: Why does this exist?
-    observing_ptr(T& x) : p_(&x) {}
+    /*! \brief Copy constructor.
+     *
+     * This allows copy construction from another observing_ptr. It takes an
+     * observing_ptr of type U instead of T to allow conversion from a pointer
+     * to a derived type to a pointer to a base type.
+     */
+    template <typename U>
+    observing_ptr(const observing_ptr<U>& ptr)
+        : p_{ptr.p_}
+    {
+    }
+
+    /*! \brief Construct from a nullptr.
+     *
+     * This ensures that when constructing with the new nullptr value in C++11
+     * the inner pointer is constructed properly.
+     */
+    observing_ptr(std::nullptr_t) : p_{nullptr} {}
 
     /*! \brief Construct from a unique_ptr.
      *
@@ -42,61 +62,137 @@ public:
      * to the same address. This does not perform a copy. The reason this does
      * not break unique_ptr's uniqueness contract is that observing_ptr is not
      * an owning smart pointer and cannot delete its internal pointer.
+     *
+     * This takes a unique_ptr of type U to allow conversion from a unique_ptr
+     * to a derived type to an observing_ptr of base type.
      */
-    observing_ptr(const std::unique_ptr<T>& ptr)
+    template <typename U>
+    observing_ptr(const std::unique_ptr<U>& ptr)
         : p_{ptr.get()}
     {
     }
 
-    observing_ptr(std::nullptr_t) : p_{nullptr} {}
-
-    template <typename U>
-    observing_ptr(const observing_ptr<U>& ptr)
-        : p_{ptr.p_}
-    {
-    }
-
+    /*! \brief Constructor from a raw pointer.
+     *
+     * This allows construction from a raw pointer, setting the observing_ptr
+     * to the same address. This allows raw pointers to be wrapped in
+     * observing_ptrs for safety.
+     *
+     * This takes a raw pointer of type U to allow conversion from a raw pointer
+     * to a derived type to an observing_ptr of base type.
+     */
     template <typename U>
     observing_ptr(U* ptr)
         : p_{ptr}
     {
     }
 
-    T const& operator*() const { return *p_; }
+    /*! \brief Constructor from a reference.
+     *
+     * This constructor exists to aid the use of certain STL algorithms.
+     *
+     * \deprecated This is hacky and doesn't fit well with other constructors
+     *             for this class. It will probably be removed once uses of it
+     *             in the engine itself are removed.
+     *
+     */
+    observing_ptr(T& ref) : p_{&ref} {}
 
-    T const* operator->() const { return p_; }
+    /*! \brief Dereference operator.
+     *
+     * This operator behaves exactly as if the pointer was a raw pointer.
+     * __This means that calling these functions on a null pointer
+     * exhibits undefined behavior.__
+     *
+     * \returns The pointer to value.
+     */
+    T& operator*() const { return *p_; }
 
-    T& operator*() { return *p_; }
+    /*! \brief Member access operator.
+     *
+     * \copydetails operator*()
+     */
+    T* operator->() const { return p_; }
 
-    T* operator->() { return p_; }
-
+    /*! \brief Explicit bool conversion operator.
+     *
+     * \return Whether the pointer is null.
+     *
+     */
     explicit operator bool() const { return p_ != nullptr; }
 
-    bool operator==(const observing_ptr& other) const { return p_ == other.p_; }
-
+    /*! \brief Equality comparison with unique_ptr.
+     *
+     * \note This performs a pointer comparison, not an object comparison.
+     *
+     * \return Whether the pointers are equal.
+     *
+     */
     bool operator==(const std::unique_ptr<T>& other) const
     {
         return p_ == other.get();
     }
 
-    bool operator==(const T* const other) const { return p_ == other; }
-
-    bool operator!=(const observing_ptr& other) const { return p_ != other.p_; }
-
+    /*! \brief Inequality comparison with unique_ptr.
+     *
+     * \note This performs a pointer comparison, not an object comparison.
+     *
+     * \return Whether the pointers are not equal.
+     *
+     */
     bool operator!=(const std::unique_ptr<T>& other) const
     {
         return p_ != other.get();
     }
 
+    /*! \brief Equality comparison with raw pointer.
+     *
+     * \note This performs a pointer comparison, not an object comparison.
+     *
+     * \return Whether the pointers are equal.
+     *
+     */
+    bool operator==(const T* const other) const { return p_ == other; }
+
+    /*! \brief Inequality comparison with raw pointer.
+     *
+     * \note This performs a pointer comparison, not an object comparison.
+     *
+     * \return Whether the pointers are not equal.
+     *
+     */
     bool operator!=(const T* const other) const { return p_ != other; }
 
+    /*! \brief Equality comparison with reference.
+     *
+     * \deprecated This is quite an odd function to have, and doesn't really
+     *             match the behaviour of the others. It may be removed once
+     *             uses of it are removed from the rest of the engine.
+     *
+     */
+    bool operator==(const T& other) const { return *p_ == other; }
+
+    /*! \brief Inequality comparison with reference.
+     *
+     * \deprecated This is quite an odd function to have, and doesn't really
+     *             match the behaviour of the others. It may be removed once
+     *             uses of it are removed from the rest of the engine.
+     *
+     */
+    bool operator!=(const T& other) const { return *p_ != other; }
+
     /*! \brief Return the contained pointer.
+     *
+     * This doesn't check for null, so it will return a null pointer if the
+     * observing_ptr is null.
      */
     T* get() { return p_; }
 
-    explicit operator T&() { return *p_; }
+    template <typename U = T>
+    operator not_bool_convertible<U>& () { return *p_; }
 
-    explicit operator T&() const { return *p_; }
+    template <typename U = T>
+    operator const not_bool_convertible<U>&() const { return *p_; }
 };
 
 template <typename T, typename U>
@@ -107,6 +203,31 @@ bool operator==(const T& lhs, const observing_ptr<U>& rhs)
 
 template <typename T, typename U>
 bool operator!=(const T& lhs, const observing_ptr<U>& rhs)
+{
+    return rhs != lhs;
+}
+
+template <typename T, typename U>
+bool operator==(const std::unique_ptr<T>& lhs, const observing_ptr<U>& rhs)
+{
+    return rhs == lhs;
+}
+
+template <typename T, typename U>
+bool operator!=(const std::unique_ptr<T>& lhs, const observing_ptr<U>& rhs)
+{
+    return rhs != lhs;
+}
+
+template <typename T, typename U>
+bool operator==(const T* const lhs, const observing_ptr<U>& rhs)
+{
+    return rhs == lhs;
+}
+
+
+template <typename T, typename U>
+bool operator!=(const T* const lhs, const observing_ptr<U>& rhs)
 {
     return rhs != lhs;
 }
--- tests/Utility/observing_ptr_test.cpp
+++ tests/Utility/observing_ptr_test.cpp
@@ -47,6 +47,15 @@ TEST_CASE("Observing Pointer Class", "[utility][observing_ptr]")
         delete x;
     }
 
+    SECTION("Dereference operator gives the contained value") {
+        auto u = std::make_unique<int>(4);
+        tank::observing_ptr<int> p1 {u};
+        REQUIRE(*p1 == 4);
+
+        const tank::observing_ptr<int> p2 {u};
+        REQUIRE(*p2 == 4);
+    }
+
     SECTION("Call through operator allowes values and functions to be accessed") {
         struct S {
             bool f() {return true;}
@@ -54,10 +63,15 @@ TEST_CASE("Observing Pointer Class", "[utility][observing_ptr]")
         };
 
         auto u = std::make_unique<S>();
-        tank::observing_ptr<S> p{u};
-        REQUIRE(p->b);
-        REQUIRE(p->f());
+        tank::observing_ptr<S> p1{u};
+        REQUIRE(p1->b);
+        REQUIRE(p1->f());
+
+        const tank::observing_ptr<S> p2 {u};
+        REQUIRE(p2->b);
+        REQUIRE(p2->f());
     }
+
 
     SECTION("Usage in boolean expressions allows implicit boolean conversion") {
         int x = 10;
@@ -66,6 +80,22 @@ TEST_CASE("Observing Pointer Class", "[utility][observing_ptr]")
 
         REQUIRE(bool(p1));
         REQUIRE(!bool(p2));
+    }
+
+    SECTION("Equality comparison operators") {
+        auto eq_unique = std::make_unique<int>(4);
+        tank::observing_ptr<int> p1 {eq_unique};
+        REQUIRE(p1 == eq_unique);
+        REQUIRE(eq_unique == p1);
+
+        auto not_eq_unique = std::make_unique<int>(4);
+        REQUIRE(p1 != not_eq_unique);
+        REQUIRE(not_eq_unique != p1);
+
+        int* raw_ptr = new int(4);
+        tank::observing_ptr<int> p2 {raw_ptr};
+        REQUIRE(p2 == raw_ptr);
+        REQUIRE(raw_ptr == p2);
     }
 
 }
