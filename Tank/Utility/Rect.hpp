9e7adb789605df4d3914a0461cede68d98767b80
David Truby <david.truby@gmail.com>
2015-01-31 17:15:38 +0100

    Comments and unit tests for Rect class.

--- Tank/Utility/Rect.hpp
+++ Tank/Utility/Rect.hpp
@@ -16,57 +16,108 @@ namespace tank
  */
 template <typename T>
 struct Rect {
-    T x, y, w, h;
-    constexpr Rect(T x = 0, T y = 0, T w = 0, T h = 0) : x(x), y(y), w(w), h(h)
+    //! \brief x coordinate of the top left point of the rectangle.
+    T x;
+
+    //! \brief y coordinate of the top left point of the rectangle.
+    T y;
+
+    //! \brief width of the rectangle.
+    T w;
+
+    //! \brief height of the rectangle.
+    T h;
+
+    /*! \brief Construct a rectangle with the given values.
+     *
+     * Defaults to an empty rectangle.
+     */
+    constexpr Rect(T x = 0, T y = 0, T w = 0, T h = 0) : x{x}, y{y}, w{w}, h{h}
     {
     }
+
+    /*! \brief Construct a rectangle with the given values, convertible to T.
+     *
+     * Implicitly converts the parameters to the type of the rectangle.
+     */
     template <typename U>
-    constexpr Rect(U x, U y, U w, U h)
-        : x(x), y(y), w(w), h(h)
+    constexpr Rect(U x, U y, U w, U h) : x(x), y(y), w(w), h(h)
     {
     }
+
+    /*! \brief Copy constructor.
+     *
+     * This works for a rectangle of any type U that is convertible to T.
+     */
     template <typename U>
     constexpr Rect(const Rect<U>& rect)
         : x(rect.x), y(rect.y), w(rect.w), h(rect.h)
     {
     }
 
+    /*! \brief Calculate intersection of two rectangles.
+     *
+     * \returns Whether the rectangles intersect.
+     */
     template <typename U>
     bool intersects(Rect<U> const& rect) const
     {
-        if (rect.x + rect.w < x or rect.y + rect.h < y or rect.x > x + w or
-            rect.y > x + h) {
-            return false;
-        }
-        return true;
+        return !(rect.x + rect.w < x || rect.y + rect.h < y ||
+                 rect.x > x + y || rect.y > x + h);
     }
 
+    /*! \brief Calculate intersection of rectangle and point.
+     *
+     * \returns Whether the point is inside the rectangle or not.
+     */
     template <typename U>
     bool intersects(Vector<U> const& point) const
     {
-        if (point.x < x or point.x > x + w or point.y < y or point.y > y + h) {
-            return false;
-        }
-        return true;
+        return !(point.x < x || point.x > x + w ||
+                 point.y < y || point.y > y + h);
     }
 };
 
+/*! \brief Equality comparison for Rects
+ *
+ * This will work for any two rects of comparable types.
+ *
+ * \relates Rect
+ */
 template <typename T, typename U>
 constexpr inline bool operator==(const Rect<T>& lhs, const Rect<U>& rhs)
 {
     return lhs.x == rhs.x and lhs.y == rhs.y and lhs.w == rhs.w and
-           lhs.h == rhs.h;
+        lhs.h == rhs.h;
 }
 
+/*! \brief Inequality comparison for Rects
+ *
+ * This will work for any two rects of comparable types.
+ *
+ * \relates Rect
+ */
 template <typename T, typename U>
 constexpr inline bool operator!=(const Rect<T>& lhs, const Rect<U>& rhs)
 {
-    return not operator==(lhs, rhs);
+    return !operator==(lhs, rhs);
 }
 
+
+/*! \brief Rectangle of floats.
+ */
 using Rectf = Rect<float>;
+
+/*! \brief Rectangle of signed integers.
+ */
 using Recti = Rect<int>;
+
+/*! \brief Rectangle of unsigned integers.
+ */
 using Rectu = Rect<unsigned int>;
+
+/*! \brief Rectangle of doubles.
+ */
 using Rectd = Rect<double>;
 }
 
---
+++ tests/Utility/Rect_test.cpp
@@ -0,0 +1,62 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include <Tank/Utility/Rect.hpp>
+
+TEST_CASE("Rectangle struct", "[utility][Rect]")
+{
+    SECTION("Default Constructor sets elements to 0") {
+        tank::Recti rect;
+        REQUIRE(rect.x == 0);
+        REQUIRE(rect.y == 0);
+        REQUIRE(rect.w == 0);
+        REQUIRE(rect.h == 0);
+    }
+
+    SECTION("Setting the values works as expected") {
+        tank::Recti rect {1, 2, 3, 4};
+        REQUIRE(rect.x == 1);
+        REQUIRE(rect.y == 2);
+        REQUIRE(rect.w == 3);
+        REQUIRE(rect.h == 4);
+    }
+
+    SECTION("Setting from convertible types works as expected") {
+        tank::Rectf rect {1u, 2u, 3u, 4u};
+        REQUIRE(rect.x == 1);
+        REQUIRE(rect.y == 2);
+        REQUIRE(rect.w == 3);
+        REQUIRE(rect.h == 4);
+    }
+
+    SECTION("Copy constructor sets relevant values") {
+        tank::Recti rect {1, 2, 3, 4};
+        tank::Recti copy {rect};
+
+        REQUIRE(rect.x == copy.x);
+        REQUIRE(rect.y == copy.y);
+        REQUIRE(rect.w == copy.w);
+        REQUIRE(rect.h == copy.h);
+    }
+
+    SECTION("Copy constructor from related type sets relevant values") {
+        tank::Recti rect {1, 2, 3, 4};
+        tank::Rectf copy {rect};
+
+        REQUIRE(rect.x == 1.f);
+        REQUIRE(rect.y == 2.f);
+        REQUIRE(rect.w == 3.f);
+        REQUIRE(rect.h == 4.f);
+    }
+
+    SECTION("Intersection of two rectangles") {
+        tank::Rectu rect1 {1, 2, 3, 4};
+        tank::Rectu rect2 {2, 3, 4, 5};
+        REQUIRE(rect1.intersects(rect2));
+    }
+
+    SECTION("Intersection of rectangle and point") {
+        tank::Rectu rect {1,2,3,4};
+        tank::Vectoru point {1,2};
+        REQUIRE(rect.intersects(point));
+    }
+}
